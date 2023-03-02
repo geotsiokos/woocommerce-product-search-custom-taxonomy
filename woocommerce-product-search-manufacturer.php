b7a2f52460d40d648895967749c5c7c559d88b36
George Tsiokos <gtsiokos@gmail.com>
2023-03-02 14:29:45 +0200

    check if taxonomy exists, added filter for including taxonomy in product taxonomies, added method docs

--- woocommerce-product-search-manufacturer.php
+++ woocommerce-product-search-manufacturer.php
@@ -38,18 +38,53 @@ if ( !defined( 'ABSPATH' ) ) {
 class WooCommerce_Product_Search_Manufacturer {
 
 	/**
+	 * Manufacturer taxonomy
+	 *
+	 * @var string
+	 */
+	private static $manufacturer_taxonomy = 'manufacturer';
+
+	/**
 	 * Boot this ...
 	 */
 	public static function init() {
-		add_filter( 'woocommerce_product_search_indexer_filter_content', array( __CLASS__, 'woocommerce_product_search_indexer_filter_content' ), 10, 3 );
+		if ( taxonomy_exists( self::$manufacturer_taxonomy ) ) {
+			add_filter( 'woocommerce_product_search_process_query_product_taxonomies', array( __CLASS__, 'woocommerce_product_search_process_query_product_taxonomies' ), 10, 2 );
+			add_filter( 'woocommerce_product_search_indexer_filter_content', array( __CLASS__, 'woocommerce_product_search_indexer_filter_content' ), 10, 3 );
+		}
 	}
 
+	/**
+	 * Add the manufacturer taxonomy to handled taxonomies.
+	 *
+	 * @param string[] $product_taxonomies
+	 * @param WP_Query $wp_query
+	 *
+	 * return string[]
+	 */
+	public static function woocommerce_product_search_process_query_product_taxonomies( $product_taxonomies, $wp_query ) {
+
+		if ( is_array( $product_taxonomies ) && !in_array( self::$taxonomy, $product_taxonomies ) ) {
+			$product_taxonomies[] = self::$taxonomy;
+		}
+		return $product_taxonomies;
+	}
+
+	/**
+	 * Add the manufacturer to content indexing so product searches for the manufacturer include related products.
+	 *
+	 * @param string $content
+	 * @param string $context
+	 * @param int $post_id
+	 *
+	 * @return string
+	 */
 	public static function woocommerce_product_search_indexer_filter_content( $content, $context, $post_id ) {
 		if ( $context === 'post_content' ) {
 
 			$manufacturers = null;
 
-			$terms = get_the_terms( $post_id, 'manufacturer' );
+			$terms = get_the_terms( $post_id, self::$manufacturer_taxonomy );
 			if ( !is_wp_error( $terms ) && !empty( $terms ) && is_array( $terms ) ) {
 				$manufacturers = array();
 				foreach ( $terms as $term ) {
@@ -58,7 +93,7 @@ class WooCommerce_Product_Search_Manufacturer {
 				$manufacturers = implode( ' ', $manufacturers );
 			}
 			if ( $manufacturers !== null && is_string( $manufacturers ) ) {
-				$content .= ' ' . $manufacturers;
+				$content .= ' ' . $manufacturers;error_log($content);
 			}
 		}
 		return $content;
